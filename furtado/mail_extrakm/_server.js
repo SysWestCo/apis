e380dc9719a9fcc3b11ef5994698cdc313ff14dd
SysWestCo <fernando@syswest.com.br>
2024-08-06 20:16:26 -0300

    ajustado conexao teste

--- furtado/mail_extrakm/_server.js
+++ furtado/mail_extrakm/_server.js
@@ -55,7 +55,18 @@ function checkForNewRecords() {
     if (err) throw err;
 
     const query = `
-      select * from gados;
+      SELECT v.id, r.rota, r.hora, 
+           date_format(v.data_hora,'%d/%m/%Y %H:%i') as data_hora,
+           v.km_chegada, v.km_rodado, v.km_extra, 
+           UPPER(v.motivo_km_extra) AS motivo_km_extra, 
+           v.litros_leite, v.dif, v.n_produtores, 
+           c.placa, m.motorista, v.acompanhante 
+    FROM controle_viagens v 
+    INNER JOIN rotas r ON v.id_rota = r.id 
+    INNER JOIN motoristas m ON v.id_motorista = m.id 
+    INNER JOIN veiculos c ON v.id_caminhao = c.id 
+    WHERE v.motivo_km_extra IS NOT NULL AND v.email_enviado = FALSE 
+    ORDER BY v.data_hora DESC;
     `;
 
     connection.query(query, (err, results) => {
@@ -68,7 +79,7 @@ function checkForNewRecords() {
             pool.getConnection((err, conn) => {
               if (err) throw err;
 
-              const updateQuery = `UPDATE gados SET email_enviado = 1 WHERE id = ?`;
+              const updateQuery = `UPDATE controle_viagens SET email_enviado = 1 WHERE id = ?`;
               conn.query(updateQuery, [recordId], (err, result) => {
                 conn.release(); // Libera a conexão de volta ao pool
                 if (err) throw err;
